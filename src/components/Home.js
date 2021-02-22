0eb04667e4998385d880566d8a16dce86c8284fc
Sutopu <sutopu@DESKTOP-I353F3M.localdomain>
2021-02-21 22:58:49 -0500

    worked on the homepage. Added images for the games.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react'
+//since images will be links to other "pages"
+import { Link, withRouter } from "react-router-dom"
 
+//paths to images
+import hangmanImage from "../images/hangman.jpg"
+import rpsImage from "../images/rps.jpg"
+import cleverbotImage from "../images/cleverbot.jpg"
 export default class Home extends Component {
     render() {
         return (
-            <div>
-                HOME
-            </div>
+            <>
+                <div>
+                    <h1 className="header">Check Out These Games!</h1>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-sm">
+                            <img src={hangmanImage} alt="hangman" className="home-image"></img>
+                        </div>
+                        <div className="col-sm">
+                            <img src={rpsImage} alt="Rock Paper Scissors" className="home-image"></img>
+                        </div>
+                        <div className="col-sm">
+                            <img src={cleverbotImage} alt="Cleverbot" className="home-image"></img>
+                        </div>
+                    </div>
+                </div>
+            </>
+
         )
     }
 }
