c5093d32cc5f2ba0e33dcc2ab10c57925aabf09f
Sutopu <sutopu@DESKTOP-I353F3M.localdomain>
2021-02-24 20:27:29 -0500

    completed hangman page.

--- src/components/hangman/Hangman.js
+++ src/components/hangman/Hangman.js
@@ -38,36 +38,76 @@ export default class Hangman extends Component {
             triedCharacters: new Set(),
             numErrors: 0,
             displayPhrase: displayPhrase,
+            gameOver: false,
         }
         this.handleChange = this.handleChange.bind(this);
     }
 
     render() {
+        if (this.state.gameOver) {
+            return (
+                <div className="text-center mt-5">
+                    <h1>GAME OVER</h1>
+                </div>
+            )
+        } else {
             return (
                 <>
-                <div className="centerEverything pr-2">
-                    <input className="form-control-sm" type="text" value={this.state.input} onChange={this.handleChange}/>
+                <div className="text-center mt-5">
+                    <div>
+                        {this.state.displayPhrase}
+                    </div>
+                    <div>
+                        <input className="form-control-sm" type="text" value={this.state.input} onChange={this.handleChange}/>
+                    </div>
+                    <div>
+                        Number of Bad Tries:&nbsp;{this.state.numErrors}
+                    </div>
                 </div>
-                <div>
-                    {this.state.displayPhrase}
+                <div className="m-5 row">
+                    <div className="col-sm text-center">
+                        <h6><strong>DESCRIPTION</strong></h6>
+                        <p>
+                            If you live under a rock and you haven't played hangman before, then you're in luck!
+                            Cause this long-winded paragraph will describe to you what hangman is. Now, read carefully,
+                            for if you do not, a man may be hung to death!
+                            <hr/>
+                            Basically, you guess random letters using the keyboard until you guess all of the letters that are
+                            in the concealed word/phrase. You only get 6 tries. 
+                        </p>
+                    </div>
+                    <div className="col-sm">
+                        <h6><strong>INSTRUCTIONS</strong></h6>
+                        <ol>
+                            <li>put in characters</li>
+                            <li>pray they are correct</li>
+                            <li>if you see game over then the game is over</li>
+                        </ol>
+                    </div>
                 </div>
                 </>
             )
+        }
     }
 
     /*
-    when the user changes value of textfield, input state variable changes accordingly.
+    when the user enters input into the text field, the state of this component is updated accordingly. This
+    is the only function that changes the state of the component.
     */
     handleChange(event) {
         const input = event.target.value;
         const char = input[input.length-1];
         const triedCharacters = this.state.triedCharacters;
         const displayPhrase = this.state.displayPhrase;
-        let updatedDisplayPhrase, updatedTriedCharacters, updatedNumErrors;
+        var updatedDisplayPhrase = this.state.displayPhrase;
+        var updatedTriedCharacters = this.state.triedCharacters;
+        var updatedNumErrors = this.state.numErrors;
+        var updatedGameOver = this.state.gameOver;
         
-
+        //if the inputted character has not yet been tried
         if (!this.state.triedCharacters.has(char)) {
             updatedTriedCharacters = triedCharacters.add(char);
+            //case where the character inputted is correct and not yet tried
             if (this.state.correctCharacters.has(char)) {
                 const charIndexes = this.getCharacterIndexes(char);
                 var tmpPhrase = displayPhrase
@@ -75,12 +115,27 @@ export default class Hangman extends Component {
                     tmpPhrase = tmpPhrase.substr(0, charIndexes[i]) + char + tmpPhrase.substr(charIndexes[i]+1);
                 }
                 updatedDisplayPhrase = tmpPhrase;
+            //case where the inputted character is an error
+            } else {
+                updatedDisplayPhrase = this.state.displayPhrase;
+                updatedNumErrors += 1;
+                if (updatedNumErrors >= 6) {
+                    updatedGameOver = true;
+                }
+                
             }
-        } 
+        //case where the inputted character has already been tried
+        } else {
+            updatedDisplayPhrase = this.state.displayPhrase;
+            updatedTriedCharacters = this.state.triedCharacters;
+        }
+        //update the state and rerender the component each time the user gives input
         this.setState({
             input: event.target.value,
             triedCharacters: updatedTriedCharacters,
-            displayPhrase: updatedDisplayPhrase
+            displayPhrase: updatedDisplayPhrase,
+            numErrors: updatedNumErrors,
+            gameOver: updatedGameOver,
         })
     }
 
